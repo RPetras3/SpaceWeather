525f6cd7b93dc15265f136e7e17a78ad998cae34
Rick Petras <ricky@petras.us>
2024-08-18 16:22:06 -0500

    initial development (#1)

---
+++ test/RPetras.SpaceWeatherLib.Tests/NoaaAuroraDataTest.cs
@@ -0,0 +1,47 @@
+namespace RPetras.SpaceWeatherLib.Tests;
+
+[TestClass]
+public class NoaaAuroraDataTest
+{
+    private const string TestData =
+        """
+        {
+          "Observation Time": "2024-08-18T18:02:00Z",
+          "Forecast Time": "2024-08-18T19:07:00Z",
+          "Data Format": "[Longitude, Latitude, Aurora]",
+          "coordinates": [
+            [0, -90, 7],
+            [0, -89, 0],
+            [0, -88, 9],
+            [0, -87, 11],
+            [0, -86, 12],
+            [0, -85, 12],
+            [0, -84, 12]
+          ]
+        }
+        """;
+
+    [TestMethod]
+    public void TestParseInvalid()
+    {
+        Assert.ThrowsException<ArgumentException>(() => NoaaAuroraData.Parse("[]"));
+    }
+
+    [TestMethod]
+    public void TestParse()
+    {
+        var data = NoaaAuroraData.Parse(TestData);
+        Assert.AreEqual(2024, data.ObservationTime.Year);
+        Assert.AreEqual(2024, data.ForecastTime.Year);
+        Assert.AreEqual(7, data.LocationData.Count);
+        Assert.IsTrue(data.LocationData.TryGetValue(new Location(0,-88), out var aurora));
+        Assert.AreEqual(9, aurora);
+    }
+
+    [TestMethod]
+    public void TestGet()
+    {
+        var data = NoaaAuroraData.Get();
+        Assert.AreEqual(360*181,data.LocationData.Count);
+    }
+}
