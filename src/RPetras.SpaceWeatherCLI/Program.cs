457c696db74960b79ba1cae6284ad1b70b0c49af
RPetras3 <ricky@petras.us>
2024-08-18 16:14:20 -0500

    initial development

---
+++ src/RPetras.SpaceWeatherCLI/Program.cs
@@ -0,0 +1,68 @@
+﻿using RPetras.SpaceWeatherLib;
+
+namespace RPetras.SpaceWeatherCLI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Print usage information upon request
+        if (args.Length == 0 || args.Contains("-h") || args.Contains("--help"))
+        {
+            Console.WriteLine(
+                """
+                Space Weather CLI
+                
+                Usage: SpaceWeatherCli <command> [options]
+                
+                Commands: 
+                  aurora <lat> <lon>        Read aurora intensity at location
+                """);
+
+            Environment.Exit(0);
+        }
+
+        try
+        {
+            switch (args[0])
+            {
+                case "aurora":
+                    DoAurora(args);
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unknown Command: {args[0]}");
+            }
+        }
+        catch (InvalidOperationException e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e.Message);
+            Console.ResetColor();
+            Environment.Exit(1);
+        }
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Unhandled Exception: {e.Message}");
+            Console.WriteLine(e);
+            Console.ResetColor();
+            throw;
+        }
+
+
+    }
+
+    private static void DoAurora(string[] args)
+    {
+        if (args.Length != 3)
+            throw new InvalidOperationException("Invalid parameters to aurora command.");
+        if (!int.TryParse(args[1], out var latitude) || latitude < -90 || latitude > 90)
+            throw new InvalidOperationException($"Invalid latitude: {args[1]}");
+        if (!int.TryParse(args[2], out var longitude) || longitude < 0 || longitude > 359)
+            throw new InvalidOperationException($"Invalid longitude: {args[2]}");
+        var data = NoaaAuroraData.Get();
+        if (!data.LocationData.TryGetValue(new Location(longitude, latitude), out var aurora))
+            throw new InvalidOperationException("Aurora data missing for specified location.");
+        Console.WriteLine($"Aurora Forecast from {data.ForecastTime} is {aurora}");
+    }
+}
---
+++ src/RPetras.SpaceWeatherLib/NoaaAuroraData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Frozen;
+using System.Text.Json.Nodes;
+
+namespace RPetras.SpaceWeatherLib;
+
+/// <summary>
+/// Integer location data
+/// </summary>
+/// <param name="Longitude">Longitude value</param>
+/// <param name="Latitude">Latitude value</param>
+public record Location(int Longitude, int Latitude);
+
+/// <summary>
+/// NOAA Aurora data
+/// </summary>
+/// <param name="ObservationTime">Observation UTC time</param>
+/// <param name="ForecastTime">Forecast UTC time</param>
+/// <param name="LocationData">Map of Location to aurora percentage</param>
+public record NoaaAuroraData(DateTime ObservationTime, DateTime ForecastTime, FrozenDictionary<Location, int> LocationData)
+{
+    /// <summary>
+    /// Parse NOAA Aurora data from json text
+    /// </summary>
+    /// <param name="jsonText">Json text to parse</param>
+    /// <returns>NoaaAuroraData instance</returns>
+    /// <exception cref="ArgumentException">Thrown on invalid json data</exception>
+    public static NoaaAuroraData Parse(string jsonText)
+    {
+        var json = JsonNode.Parse(jsonText) ?? throw new ArgumentException("Invalid NOAA json data", nameof(jsonText));
+        var observationTimeText = json["Observation Time"]?.ToString() ?? throw new ArgumentException("Invalid NOAA json observation time", nameof(jsonText));
+        var forecastTimeText = json["Forecast Time"]?.ToString() ?? throw new ArgumentException("Invalid NOAA json forecast time", nameof(jsonText));
+        var observationTime = DateTime.Parse(observationTimeText);
+        var forecastTime = DateTime.Parse(forecastTimeText);
+        var coordinates = json["coordinates"] as JsonArray ?? throw new ArgumentException("Invalid NOAA json coordinate array");
+        var coordinatesDictionary = new Dictionary<Location, int>();
+        foreach (var coordinate in coordinates.OfType<JsonArray>())
+        {
+            var data = coordinate.GetValues<int>().ToArray();
+            if (data.Length != 3)
+                throw new ArgumentException("Invalid NOAA json coordinate entry");
+            coordinatesDictionary.Add(new Location(data[0], data[1]), data[2]);
+        }
+
+        return new NoaaAuroraData(observationTime, forecastTime, coordinatesDictionary.ToFrozenDictionary());
+    }
+    public static NoaaAuroraData Get()
+    {
+        // TODO get NOAA data
+        using var client = new HttpClient();
+        var response = client.GetAsync("https://services.swpc.noaa.gov/json/ovation_aurora_latest.json").Result;
+        // Read data into NoaaAuroraData
+        return Parse(response.Content.ReadAsStringAsync().Result);
+    }
+}
