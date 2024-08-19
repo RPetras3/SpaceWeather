7dd127394b10d46c403451f15312142ed4aa5d37
Rick Petras <ricky@petras.us>
2024-08-18 19:20:13 -0500

    Code cleanup, commenting, and improved handling of badly formatted dates (#6)

--- src/RPetras.SpaceWeatherLib/NoaaAuroraData.cs
+++ src/RPetras.SpaceWeatherLib/NoaaAuroraData.cs
@@ -5,39 +5,53 @@ using System.Text.Json.Nodes;
 namespace RPetras.SpaceWeatherLib;
 
 /// <summary>
-/// Integer location data
+///     Integer location data
 /// </summary>
 /// <param name="Longitude">Longitude value</param>
 /// <param name="Latitude">Latitude value</param>
 public record Location(int Longitude, int Latitude);
 
 /// <summary>
-/// NOAA Aurora data
+///     NOAA Aurora data
 /// </summary>
 /// <param name="ObservationTime">Observation UTC time</param>
 /// <param name="ForecastTime">Forecast UTC time</param>
 /// <param name="LocationData">Map of Location to aurora percentage</param>
-public record NoaaAuroraData(DateTime ObservationTime, DateTime ForecastTime, FrozenDictionary<Location, int> LocationData)
+public record NoaaAuroraData(
+    DateTime ObservationTime,
+    DateTime ForecastTime,
+    FrozenDictionary<Location, int> LocationData)
 {
     /// <summary>
-    /// NOAA Url for aurora data
+    ///     NOAA Url for aurora data
     /// </summary>
     private const string NoaaUrl = "https://services.swpc.noaa.gov/json/ovation_aurora_latest.json";
 
     /// <summary>
-    /// Parse NOAA Aurora data from json text
+    ///     Parse NOAA Aurora data from json text
     /// </summary>
     /// <param name="jsonText">Json text to parse</param>
     /// <returns>NoaaAuroraData instance</returns>
     /// <exception cref="ArgumentException">Thrown on invalid json data</exception>
     public static NoaaAuroraData Parse(string jsonText)
     {
-        var json = JsonNode.Parse(jsonText) as JsonObject ?? throw new ArgumentException("Invalid NOAA json data", nameof(jsonText));
-        var observationTimeText = json["Observation Time"]?.ToString() ?? throw new ArgumentException("Invalid NOAA json observation time", nameof(jsonText));
-        var forecastTimeText = json["Forecast Time"]?.ToString() ?? throw new ArgumentException("Invalid NOAA json forecast time", nameof(jsonText));
-        var observationTime = DateTime.Parse(observationTimeText, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
-        var forecastTime = DateTime.Parse(forecastTimeText, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
-        var coordinates = json["coordinates"] as JsonArray ?? throw new ArgumentException("Invalid NOAA json coordinate array");
+        // Parse the JSON text into a JsonObject
+        var json = JsonNode.Parse(jsonText) as JsonObject ??
+                   throw new ArgumentException("Invalid NOAA json data", nameof(jsonText));
+
+        // Extract and Parse the observation time
+        if (!DateTime.TryParse(json["Observation Time"]?.ToString(), CultureInfo.InvariantCulture,
+                DateTimeStyles.RoundtripKind, out var observationTime))
+            throw new ArgumentException("Invalid NOAA json observation time", nameof(jsonText));
+
+        // Extract and Parse the forecast time
+        if (!DateTime.TryParse(json["Forecast Time"]?.ToString(), CultureInfo.InvariantCulture,
+                DateTimeStyles.RoundtripKind, out var forecastTime))
+            throw new ArgumentException("Invalid NOAA json forecast time", nameof(jsonText));
+
+        // Extract and Parse the coordinates array
+        var coordinates = json["coordinates"] as JsonArray ??
+                          throw new ArgumentException("Invalid NOAA json coordinate array");
         var coordinatesDictionary = new Dictionary<Location, int>();
         foreach (var coordinate in coordinates.OfType<JsonArray>())
         {
@@ -47,20 +61,39 @@ public record NoaaAuroraData(DateTime ObservationTime, DateTime ForecastTime, Fr
             coordinatesDictionary.Add(new Location(data[0], data[1]), data[2]);
         }
 
+        // Returns a new instance of NoaaAuroraData with the data retrieved from NOAA
         return new NoaaAuroraData(observationTime, forecastTime, coordinatesDictionary.ToFrozenDictionary());
     }
 
     /// <summary>
-    /// Get the NOAA aurora data
+    ///     Get the NOAA aurora data
     /// </summary>
     /// <param name="url">NOAA url or null for default</param>
     /// <returns>NoaaAuroraData instance</returns>
-    public static NoaaAuroraData Get(string? url = null)
+    public static async Task<NoaaAuroraData> GetAsync(string? url = null)
     {
+        // Use default URL if none is provided
         url ??= NoaaUrl;
+
+        // Create an HttpClient instance to send the request
         using var client = new HttpClient();
-        var response = client.GetAsync(url).Result;
+        HttpResponseMessage response;
+        try
+        {
+            // Send the GET request and ensure the response is successful
+            response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (HttpRequestException e)
+        {
+            // Throw an exception if there is an error fetching the data
+            throw new InvalidOperationException("Error fetching NOAA data", e);
+        }
+
+        // Read the response content as a string
+        var jsonText = await response.Content.ReadAsStringAsync();
+
         // Read data into NoaaAuroraData
-        return Parse(response.Content.ReadAsStringAsync().Result);
+        return Parse(jsonText);
     }
 }
