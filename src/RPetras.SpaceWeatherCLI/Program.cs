39947c432ae07801dbbb01c1d089ee1e01e62718
Rick Petras <ricky@petras.us>
2024-08-18 19:18:11 -0500

    Code cleanup, commenting, and improved handling of badly formatted dates

--- src/RPetras.SpaceWeatherCLI/Program.cs
+++ src/RPetras.SpaceWeatherCLI/Program.cs
@@ -2,19 +2,27 @@
 
 namespace RPetras.SpaceWeatherCLI;
 
+/// <summary>
+///     The Space Weather CLI application class
+/// </summary>
 public static class Program
 {
+    /// <summary>
+    ///     The main method, which is the entry point of the application.
+    /// </summary>
+    /// <param name="args">Command line arguments</param>
     public static void Main(string[] args)
     {
+        // Display a welcome message
+        Console.WriteLine("Welcome to the Space Weather CLI");
+
         // Print usage information upon request
         if (args.Length == 0 || args.Contains("-h") || args.Contains("--help"))
         {
             Console.WriteLine(
                 """
-                Space Weather CLI
-                
                 Usage: SpaceWeatherCli <command> [options]
-                
+
                 Commands: 
                   aurora <lat> <lon>        Read aurora intensity at location
                 """);
@@ -24,17 +32,21 @@ public static class Program
 
         try
         {
+            // Traverse the arguments for commands to run
             switch (args[0])
             {
+                // Run the aurora prediction command for given location
                 case "aurora":
                     DoAurora(args);
                     break;
+                // Throw exception for unknown commands
                 default:
                     throw new InvalidOperationException($"Unknown Command: {args[0]}");
             }
         }
         catch (InvalidOperationException e)
         {
+            // Handle known exceptions and exit
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(e.Message);
             Console.ResetColor();
@@ -42,27 +54,42 @@ public static class Program
         }
         catch (Exception e)
         {
+            // Handle unknown exceptions
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"Unhandled Exception: {e.Message}");
             Console.WriteLine(e);
             Console.ResetColor();
             throw;
         }
-
-
     }
 
+    /// <summary>
+    ///     Handle the aurora command from the CLI
+    /// </summary>
+    /// <param name="args">command line arguments passed</param>
+    /// <exception cref="InvalidOperationException">Thrown on invalid arguments or NOAA data errors</exception>
     private static void DoAurora(string[] args)
     {
+        // Ensure we have the correct number of arguments for this command
         if (args.Length != 3)
             throw new InvalidOperationException("Invalid parameters to aurora command.");
+
+        // Ensure we have a proper latitude value
         if (!int.TryParse(args[1], out var latitude) || latitude < -90 || latitude > 90)
             throw new InvalidOperationException($"Invalid latitude: {args[1]}");
+
+        // Ensure we have a proper longitude value
         if (!int.TryParse(args[2], out var longitude) || longitude < 0 || longitude > 359)
             throw new InvalidOperationException($"Invalid longitude: {args[2]}");
-        var data = NoaaAuroraData.Get();
+
+        // Retrieve the NOAA data from their server
+        var data = NoaaAuroraData.GetAsync().Result;
+
+        // Check the retrieved NOAA data for a valid aurora prediction value
         if (!data.LocationData.TryGetValue(new Location(longitude, latitude), out var aurora))
             throw new InvalidOperationException("Aurora data missing for specified location.");
+
+        // Print out our aurora prediction results
         Console.WriteLine($"Aurora Forecast from {data.ForecastTime} is {aurora}");
     }
 }
--- test/RPetras.SpaceWeatherLib.Tests/NoaaAuroraDataTest.cs
+++ test/RPetras.SpaceWeatherLib.Tests/NoaaAuroraDataTest.cs
@@ -2,27 +2,15 @@ using System.Text.Json;
 
 namespace RPetras.SpaceWeatherLib.Tests;
 
+/// <summary>
+///     Unit tests for the NoaaAuroraData class
+/// </summary>
 [TestClass]
 public class NoaaAuroraDataTest
 {
-    private const string TestData =
-        """
-        {
-          "Observation Time": "2024-08-18T18:02:00Z",
-          "Forecast Time": "2024-08-18T19:07:00Z",
-          "Data Format": "[Longitude, Latitude, Aurora]",
-          "coordinates": [
-            [0, -90, 7],
-            [0, -89, 0],
-            [0, -88, 9],
-            [0, -87, 11],
-            [0, -86, 12],
-            [0, -85, 12],
-            [0, -84, 12]
-          ]
-        }
-        """;
-
+    /// <summary>
+    ///     Test invalid json data
+    /// </summary>
     [TestMethod]
     public void TestParseInvalid()
     {
@@ -37,27 +25,86 @@ public class NoaaAuroraDataTest
         }
     }
 
+    /// <summary>
+    ///     Test for incompatible json data
+    /// </summary>
     [TestMethod]
     public void TestParseIncorrectType()
     {
         Assert.ThrowsException<ArgumentException>(() => NoaaAuroraData.Parse("[]"));
     }
 
+    /// <summary>
+    ///     Test for badly formatted observation time
+    /// </summary>
+    [TestMethod]
+    public void TestParseInvalidObservationTime()
+    {
+        Assert.ThrowsException<ArgumentException>(
+            () => NoaaAuroraData.Parse(
+                """
+                {
+                  "Observation Time": "Invalid-Value",
+                  "Forecast Time": "2024-08-18T19:07:00Z",
+                  "coordinates": []
+                }
+                """));
+    }
+
+    /// <summary>
+    ///     Test for badly formatted forecast time
+    /// </summary>
+    [TestMethod]
+    public void TestParseInvalidForecastTime()
+    {
+        Assert.ThrowsException<ArgumentException>(
+            () => NoaaAuroraData.Parse(
+                """
+                {
+                  "Observation Time": "2024-08-18T18:02:00Z",
+                  "Forecast Time": "Invalid-Value",
+                  "coordinates": []
+                }
+                """));
+    }
+
+    /// <summary>
+    ///     Test parsing of valid data
+    /// </summary>
     [TestMethod]
     public void TestParse()
     {
-        var data = NoaaAuroraData.Parse(TestData);
+        var data = NoaaAuroraData.Parse(
+            """
+            {
+              "Observation Time": "2024-08-18T18:02:00Z",
+              "Forecast Time": "2024-08-18T19:07:00Z",
+              "Data Format": "[Longitude, Latitude, Aurora]",
+              "coordinates": [
+                [0, -90, 7],
+                [0, -89, 0],
+                [0, -88, 9],
+                [0, -87, 11],
+                [0, -86, 12],
+                [0, -85, 12],
+                [0, -84, 12]
+              ]
+            }
+            """);
         Assert.AreEqual(2024, data.ObservationTime.Year);
         Assert.AreEqual(2024, data.ForecastTime.Year);
         Assert.AreEqual(7, data.LocationData.Count);
-        Assert.IsTrue(data.LocationData.TryGetValue(new Location(0,-88), out var aurora));
+        Assert.IsTrue(data.LocationData.TryGetValue(new Location(0, -88), out var aurora));
         Assert.AreEqual(9, aurora);
     }
 
+    /// <summary>
+    ///     Test reading from official NOAA website
+    /// </summary>
     [TestMethod]
     public void TestGet()
     {
-        var data = NoaaAuroraData.Get();
-        Assert.AreEqual(360*181,data.LocationData.Count);
+        var data = NoaaAuroraData.GetAsync().Result;
+        Assert.AreEqual(360 * 181, data.LocationData.Count);
     }
 }
