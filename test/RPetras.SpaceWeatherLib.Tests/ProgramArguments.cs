e7beeee9663fb9026071d12caac78528c113e788
Rick Petras <ricky@petras.us>
2024-08-25 16:20:17 -0500

    Add Program Runners for unit testing

---
+++ test/RPetras.SpaceWeatherLib.Tests/ProgramArguments.cs
@@ -0,0 +1,16 @@
+﻿namespace RPetras.SpaceWeatherLib.Tests;
+
+[TestClass]
+public class ProgramArguments
+{
+    [TestMethod]
+    public void TestBadArgument()
+    {
+        // Run the command line application with bad argument
+        var exitCode = Runner.Run(out var output, "dotnet", "RPetras.SpaceWeatherCLI.dll", "invalid-parameter");
+
+        // Verify program failed
+        Assert.AreNotEqual(0, exitCode);
+        Assert.IsTrue(output.Contains("Unknown Command:"));
+    }
+}
---
+++ test/RPetras.SpaceWeatherLib.Tests/ProgramAuroraTest.cs
@@ -0,0 +1,37 @@
+﻿namespace RPetras.SpaceWeatherLib.Tests;
+
+[TestClass]
+public class ProgramAuroraTest
+{
+    [TestMethod]
+    public void TestAuroraCommand()
+    {
+        // Run the command line application with bad argument
+        var exitCode = Runner.Run(
+            out var output,
+            "dotnet",
+            "RPetras.SpaceWeatherCLI.dll",
+            "aurora",
+            "45",
+            "45");
+
+        // Verify program succeeded
+        Assert.AreEqual(0, exitCode);
+        Assert.IsTrue(output.Contains("Aurora Forecast from"));
+    }
+
+    [TestMethod]
+    public void TestAuroraCommandMissingArguments()
+    {
+        // Run the command line application with no arguments for aurora command
+        var exitCode = Runner.Run(
+            out var output,
+            "dotnet",
+            "RPetras.SpaceWeatherCLI.dll",
+            "aurora");
+
+        // Verify program failed
+        Assert.AreNotEqual(0, exitCode);
+        Assert.IsTrue(output.Contains("Invalid parameters"));
+    }
+}
