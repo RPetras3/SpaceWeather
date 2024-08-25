555b58176f0cefe350925cc8df573327318db2b5
Rick Petras <ricky@petras.us>
2024-08-25 16:31:22 -0500

    Add Program Runners for unit testing (#12)
    * Add Program Runners for unit testing
    
    * Add manual workflow action
    
    * update workflow triggers

---
+++ test/RPetras.SpaceWeatherLib.Tests/ProgramHelpTest.cs
@@ -0,0 +1,38 @@
+﻿namespace RPetras.SpaceWeatherLib.Tests;
+
+[TestClass]
+public class ProgramHelpTest
+{
+    [TestMethod]
+    public void TestNoArgs()
+    {
+        // Run the command line application with no args
+        var exitCode = Runner.Run(out var output, "dotnet", "RPetras.SpaceWeatherCLI.dll");
+
+        // Verify program exited correctly
+        Assert.AreEqual(0, exitCode);
+        Assert.IsTrue(output.Contains("Usage:"));
+    }
+
+    [TestMethod]
+    public void TestHelpShort()
+    {
+        // Run the command line application with short help arg
+        var exitCode = Runner.Run(out var output, "dotnet", "RPetras.SpaceWeatherCLI.dll", "-h");
+
+        // Verify program exited correctly
+        Assert.AreEqual(0, exitCode);
+        Assert.IsTrue(output.Contains("Usage:"));
+    }
+
+    [TestMethod]
+    public void TestHelpLong()
+    {
+        // Run the command line application with long help arg
+        var exitCode = Runner.Run(out var output, "dotnet", "RPetras.SpaceWeatherCLI.dll", "--help");
+
+        // Verify program exited correctly
+        Assert.AreEqual(0, exitCode);
+        Assert.IsTrue(output.Contains("Usage:"));
+    }
+}
